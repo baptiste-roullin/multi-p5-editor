83e5e10fd22254cd7eb0a946206932b0b265e318
Baptiste <baptiste@roullin.net>
2023-12-03 21:48:33 +0100

    fix script ?

--- new_files.sh
+++ new_files.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 #cd "${0%/*}"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
 ls ./src/views/ | sed -e 's/\..*$//'| jq -R -s -c 'split("\n")' > src/files.json
