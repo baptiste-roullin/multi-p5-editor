29af83300852dfe8358d72c6ff8582886d614176
Baptiste <baptiste@roullin.net>
2024-02-04 23:22:04 +0100

    clean

--- src/views/circle_wrapping_inwards.vue
+++ src/views/circle_wrapping_inwards.vue
@@ -1,17 +1,41 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import { globalInit, usefulHeight, usefulWidth } from '@/globalUtils'
-import record from '@/record'
+import recordVideo from '@/recordVideo'
 onMounted(() => {
+
+	const event = new Event("cycled")
+	var currentTan = 0
+	var pastTan = 0
+	var numberOfPhases = 0
+	var recording: boolean = false
+
+	function recordVideoCircle(trigFn) {
+		if (!currentTan) {
+			pastTan = 0
+		} {
+			pastTan = currentTan
+		}
+		currentTan = trigFn
+
+		if (currentTan < 0 && pastTan > 0) {
+			console.log(numberOfPhases)
+			numberOfPhases++
+
+		}
+		if (!recording) {
+			recordVideo()
+			recording = true
+		}
+
+		if (numberOfPhases > 1) {
+			document.dispatchEvent(event)
+		}
+	}
 
 	function draw() {
 		background(2, 2, 2)
-		const dimension = Math.min(
-			usefulHeight,
-			usefulWidth,
-		)
 		fill(255)
-		//fill(0, 0, 0, 0)
 		stroke(255, 255, 255)
 
 		let sin = Math.sin(Date.now() / 5000)
@@ -27,42 +51,13 @@ onMounted(() => {
 			0,
 			2 * Math.PI,
 			//mode
-
 		)
 
-		record_circle(tan)
+		recordVideoCircle(tan)
 	}
 	globalInit(draw, true)
 })
 
-const event = new Event("cycled")
-var currentTan = 0
-var pastTan = 0
-var numberOfPhases = 0
-var recording: boolean = false
-
-function record_circle(trigFn) {
-	if (!currentTan) {
-		pastTan = 0
-	} {
-		pastTan = currentTan
-	}
-	currentTan = trigFn
-
-	if (currentTan < 0 && pastTan > 0) {
-		console.log(numberOfPhases)
-		numberOfPhases++
-
-	}
-	if (!recording) {
-		var mediaRecorder = record()
-		recording = true
-	}
-
-	if (numberOfPhases > 1) {
-		document.dispatchEvent(event)
-	}
-}
 </script>
 
 <template>
--- src/views/circle_wrapping_outwards.vue
+++ src/views/circle_wrapping_outwards.vue
@@ -1,20 +1,39 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import { globalInit, usefulHeight, usefulWidth } from '@/globalUtils'
-import record from '@/record'
+import recordVideo from '@/recordVideo'
 
+const event = new Event("cycled")
+var currentTan = 0
+var pastTan = 0
+var numberOfPhases = 0
+var recording: boolean = false
+
+function recordVideoCircle(trigFn) {
+	if (!currentTan) {
+		pastTan = 0
+	} {
+		pastTan = currentTan
+	}
+	currentTan = trigFn
+
+	if (currentTan < 0 && pastTan > 0) {
+		console.log(numberOfPhases)
+		numberOfPhases++
+	}
+	if (!recording) {
+		recordVideo()
+		recording = true
+	}
+	if (numberOfPhases > 1) {
+		document.dispatchEvent(event)
+	}
+}
 
 onMounted(() => {
 
-	console.log("mount")
-
 	function draw() {
-
 		background(2, 2, 2)
-		const dimension = Math.min(
-			usefulHeight,
-			usefulWidth,
-		)
 		fill(255)
 
 		let tan = Math.tan(Date.now() / 1000)
@@ -30,40 +49,10 @@ onMounted(() => {
 			0,
 			2 * Math.PI,
 		)
-		record_circle(tan)
+		recordVideoCircle(tan)
 	}
 	globalInit(draw, true)
 })
-
-const event = new Event("cycled")
-var currentTan = 0
-var pastTan = 0
-var numberOfPhases = 0
-var recording: boolean = false
-
-
-function record_circle(trigFn) {
-	if (!currentTan) {
-		pastTan = 0
-	} {
-		pastTan = currentTan
-	}
-	currentTan = trigFn
-
-	if (currentTan < 0 && pastTan > 0) {
-		console.log(numberOfPhases)
-		numberOfPhases++
-
-	}
-	if (!recording) {
-		var mediaRecorder = record()
-		recording = true
-	}
-
-	if (numberOfPhases > 1) {
-		document.dispatchEvent(event)
-	}
-}
 
 </script>
 
